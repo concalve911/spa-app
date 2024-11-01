e4de67ee906b435542e1792e21c9ecc515fe360a
concalve911 <aleksandrgarkusha911@gmail.com>
2024-11-01 16:13:57 +0100

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import Inspect from "vite-plugin-inspect";
 import OpenIde from "vite-inspector";
-import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/spa-app/",
